6044ef8905fda798f4c8f0096fecd5ffb2439133
Hossein Hashemi <91213707+hashemi-hossein@users.noreply.github.com>
2024-11-17 16:16:37 +0100

    improve HomeScreen.kt

--- app/src/main/java/ara/hossein/androidtraining/speedmath/screen/HomeScreen.kt
+++ app/src/main/java/ara/hossein/androidtraining/speedmath/screen/HomeScreen.kt
@@ -1,64 +1,65 @@
 package ara.hossein.androidtraining.speedmath.screen
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlin.random.Random
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    var firstNumber by remember { mutableIntStateOf(1) }
+    var secondNumber by remember { mutableIntStateOf(1) }
+    var userInput by remember { mutableStateOf("") }
+    var result by remember { mutableStateOf("") }
+
+    fun generateNumbers() {
+        firstNumber = Random(seed = System.currentTimeMillis()).nextInt(1, 10)
+        secondNumber = Random(seed = System.currentTimeMillis()).nextInt(1, 10)
+    }
+
+    fun checkAnswer(userInput: String): Boolean {
+        val correctAnswer = firstNumber + secondNumber
+        return userInput.toIntOrNull() == correctAnswer
+    }
+
+    LaunchedEffect(Unit) {
+        generateNumbers()
+    }
+
     Box(modifier = Modifier.fillMaxSize()) {
-        Column(Modifier.align(Alignment.Center)) {
-
-            var firstNumber by remember { mutableIntStateOf(1) }
-            var secondNumber by remember { mutableIntStateOf(1) }
-            var userInput by remember { mutableStateOf("") }
-            var result by remember { mutableStateOf("") }
-
-            fun generateNumbers() {
-                firstNumber = Random(seed = System.currentTimeMillis()).nextInt(1, 10)
-                secondNumber = Random(seed = System.currentTimeMillis()).nextInt(1, 10)
-            }
-
-            fun checkAnswer(userInput: String): Boolean {
-                val correctAnswer = firstNumber + secondNumber
-                return userInput.toIntOrNull() == correctAnswer
-            }
-
-            LaunchedEffect(Unit) {
-                generateNumbers()
-            }
-
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text(
-                    text = "$firstNumber + $secondNumber",
-                    style = MaterialTheme.typography.headlineMedium,
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                OutlinedTextField(
-                    value = userInput,
-                    onValueChange = { userInput = it },
-                    label = { Text("Your Answer") }
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(onClick = {
-                    if (checkAnswer(userInput)) {
-                        result = "Correct!"
-                        generateNumbers()
-                        userInput = ""
-                    } else {
-                        result = "Incorrect, try again."
-                    }
-                }) {
-                    Text("Submit")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.align(Alignment.Center),
+        ) {
+            Text(
+                text = "$firstNumber + $secondNumber",
+                style = MaterialTheme.typography.headlineMedium,
+            )
+            OutlinedTextField(
+                value = userInput,
+                onValueChange = { userInput = it },
+                label = { Text("Your Answer") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            )
+            Button(onClick = {
+                if (checkAnswer(userInput)) {
+                    result = "Correct!"
+                    generateNumbers()
+                    userInput = ""
+                } else {
+                    result = "Incorrect, try again."
                 }
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(text = result, style = MaterialTheme.typography.bodyLarge)
+            }) {
+                Text("Submit")
             }
+            Text(text = result, style = MaterialTheme.typography.bodyLarge)
         }
     }
 }
